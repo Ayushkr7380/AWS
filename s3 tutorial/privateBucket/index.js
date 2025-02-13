e363898799104ea3bde66cbbbebc3fab7bae05fa
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-13 14:04:12 +0530

    put object signed url

--- s3 tutorial/privateBucket/index.js
+++ s3 tutorial/privateBucket/index.js
@@ -1,33 +1,43 @@
-import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import {GetObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv  from "dotenv";
 
 dotenv.config();
-
-try {
     
-    const s3Client = new S3Client({
+const s3Client = new S3Client({
         region:"ap-south-1",
         credentials:{
             accessKeyId:process.env.accessKeyId,
             secretAccessKey:process.env.secretAccessKey
         }
-    });
+});
 
-    async function getObjectURL(key){
+//Signed URL GET OBJECT
+async function getObjectURL(key){
     const command = new GetObjectCommand({
         Bucket:"ayush-private",
         Key:key,
     });
-
     const url = await getSignedUrl(s3Client,command);
     return url;
 }
 
+//Signed URL PUT OBJECT
+async function putObject(filename,contentType){
+    const command = new PutObjectCommand({
+        Bucket:'ayush-private',
+        Key:`uploads/user-uploads/${filename}`,
+        ContentType:contentType
+    })
+    const url = getSignedUrl(s3Client,command);
+    return url;
+}
+
+
 async function init(){
-    console.log("Url is : ", await getObjectURL("f1.jpg"));
+    console.log("Url is : ", await getObjectURL("uploads/user-uploads/video-1739434193984.mp4"));
+    // console.log('URL for upoading ',await putObject(`image-${Date.now()}.jpg`,"image/jpg"));
+    // console.log('URL for upoading ',await putObject(`video-${Date.now()}.mp4`,"video/mp4"));
 }
+
 init();
-} catch (error) {
-    console.log(error.message);
-}
